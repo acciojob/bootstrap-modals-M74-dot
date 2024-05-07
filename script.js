1d65859499cb14a2b9fabc5d6c2cd0fca6267aac
Manisha Mali <82174253+M74-dot@users.noreply.github.com>
2024-05-07 12:46:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,30 +1,15 @@
 document.addEventListener("DOMContentLoaded", function() {
-  var myCarousel = new bootstrap.Carousel(document.getElementById('myCarousel'), {
-    interval: false // Disable automatic sliding
-  });
+    var openModalButton = document.getElementById("open-modal");
+    var modal = document.getElementById("newton-modal");
+    var closeModalButtons = document.querySelectorAll(".close-modal");
 
-  var leftControl = document.querySelector('.carousel-control-prev');
-  var rightControl = document.querySelector('.carousel-control-next');
-
-  leftControl.addEventListener('click', function() {
-    myCarousel.prev();
-  });
-
-  rightControl.addEventListener('click', function() {
-    myCarousel.next();
-  });
-
-  var openModalButton = document.getElementById("open-modal");
-  var modal = document.getElementById("newton-modal");
-  var closeModalButtons = document.querySelectorAll(".close-modal");
-
-  openModalButton.addEventListener("click", function() {
-    modal.style.display = "block";
-  });
-
-  closeModalButtons.forEach(function(button) {
-    button.addEventListener("click", function() {
-      modal.style.display = "none";
+    openModalButton.addEventListener("click", function() {
+        modal.style.display = "block";
     });
-  });
+
+    closeModalButtons.forEach(function(button) {
+        button.addEventListener("click", function() {
+            modal.style.display = "none";
+        });
+    });
 });
