47b6c56d94ca56b42537cbe03b4090e03cc6a0da
Manisha Mali <82174253+M74-dot@users.noreply.github.com>
2024-05-07 12:42:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 document.addEventListener("DOMContentLoaded", function() {
+  var myCarousel = new bootstrap.Carousel(document.getElementById('myCarousel'), {
+    interval: false // Disable automatic sliding
+  });
+
+  var leftControl = document.querySelector('.carousel-control-prev');
+  var rightControl = document.querySelector('.carousel-control-next');
+
+  leftControl.addEventListener('click', function() {
+    myCarousel.prev();
+  });
+
+  rightControl.addEventListener('click', function() {
+    myCarousel.next();
+  });
+
   var openModalButton = document.getElementById("open-modal");
   var modal = document.getElementById("newton-modal");
   var closeModalButtons = document.querySelectorAll(".close-modal");
