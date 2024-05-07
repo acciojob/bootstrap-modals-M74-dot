882d45c414bd30046a339c6b580e7b86ce3528de
Manisha Mali <82174253+M74-dot@users.noreply.github.com>
2024-05-07 12:26:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your code here
+var myModal = new bootstrap.Modal(document.getElementById('newton-modal'));
+
+  document.getElementById('open-modal').addEventListener('click', function () {
+    myModal.show();
+  });
